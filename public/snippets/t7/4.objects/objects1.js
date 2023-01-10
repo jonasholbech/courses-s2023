8ef7f33613f65b4653a6dc8e1180c8cd5219403a
Jonas Holbech <jh@jonasholbech.dk>
2023-01-10 08:53:10 +0100

    update git tut

--- public/snippets/t7/4.objects/objects1.js
+++ public/snippets/t7/4.objects/objects1.js
@@ -2,5 +2,5 @@ const car = {
   make: "Peugeot",
   model: "5008",
   wheels: 4,
-  passengers: ["Jonas", "Gitte", "Storm", "Birk", "liv"],
+  passengers: ["Jonas", "Gitte", "Storm", "Birk", "Liv"],
 };
